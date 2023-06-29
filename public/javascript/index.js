26d71a0ce7e887e8045b1f22e9f9124e8f38dc9e
Filipa Barros <barrosfilipa.f@gmail.com>
2023-06-29 17:47:54 +0200

    done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,35 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+// charactersAPI.getFullList().then((resp) => console.log(resp));
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+// axios.get("http://localhost:8000/characters").then((resp) => console.log(resp));
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-  });
+window.addEventListener("load", () => {
+  charactersAPI.getFullList().then((resp) => console.log(resp));
+});
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {});
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {});
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {});
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {});
+});
 
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+document.getElementById("get-country-btn").addEventListener("click", () => {
+  const userInput = document.getElementById("country-name-input").value;
+  getCountryInfo(userInput);
 });
